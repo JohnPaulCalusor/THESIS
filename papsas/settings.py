68e63d9bb042e3017ee762b432b25f75b187af83
CastroClarence <21-00390@g.batstate-u.edu.ph>
2024-08-15 22:31:37 +0800

    postgresql & superuser

--- papsas/settings.py
+++ papsas/settings.py
@@ -76,10 +76,17 @@ WSGI_APPLICATION = 'papsas.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "macbookairm1",
+        "USER": "postgres",
+        "PASSWORD": "password",
+        "HOST": "localhost",
+        "PORT": "",
     }
+
+    # add new postgresql database
+    
 }
 
 
