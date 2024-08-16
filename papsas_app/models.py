c4897e44cf97b19b5f9d86bf63c667285aa5bcae
CastroClarence <21-00390@g.batstate-u.edu.ph>
2024-08-16 13:28:28 +0800

    link to register

--- papsas_app/models.py
+++ papsas_app/models.py
@@ -7,7 +7,7 @@ class User(AbstractUser):
     mobileNum = models.CharField(max_length=16)
     region = models.CharField(max_length=32)
     address = models.CharField(max_length=32)
-    occupation = models.CharField(max_length=16)
+    occupation = models.CharField(max_length=32)
     age = models.IntegerField(null=True)
     memType = models.CharField(max_length=32)
     birthdate = models.DateField(null=True)
