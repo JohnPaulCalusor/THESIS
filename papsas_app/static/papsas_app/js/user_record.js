4ad2d97bcbb42e3d4bac9f87844c8af49d375783
Kris Nathaniel Dimaapi <21-00385@g.batstate-u.edu.ph>
2024-10-10 13:25:46 +0800

    updated records

--- papsas_app/static/papsas_app/js/user_record.js
+++ papsas_app/static/papsas_app/js/user_record.js
@@ -24,16 +24,16 @@ function fetchUserInfo(userId) {
             document.getElementById('id_birthdate').value = data.birthdate;
             document.getElementById('id_institution').value = data.institution;
 
-            const container = document.getElementById('popup_container')
+            const container = document.getElementById('details-container')
             const form = container.querySelector('form');
             form.action = `/user/update/${userId}`;
             })
         .catch(error => console.error('Error fetching user info:', error));
     
-    document.querySelector('#popup_container').style.display = 'block';
+    document.querySelector('#details-container').style.display = 'block';
 }
 function showPopup() {
-    document.querySelector('#popup_container').style.display = 'block';
+    document.querySelector('#details-container').style.display = 'block';
     document.body.insertAdjacentHTML('beforeend', '<div class="popup-overlay"></div>');
 }
 
@@ -48,7 +48,7 @@ function closePopup() {
     document.getElementById('id_age').value =  "";
     document.getElementById('id_birthdate').value =  "";
     document.getElementById('id_institution').value =  "";
-    document.querySelector('#popup_container').style.display = 'none';
+    document.querySelector('#details-container').style.display = 'none';
     const overlay = document.querySelector('.popup-overlay');
     if (overlay) {
         overlay.remove();
